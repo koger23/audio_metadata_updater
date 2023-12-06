5439c386bd77c065ab1029afb25e6b2b2377a21f
koger23 <koger23@gmail.com>
2023-12-06 18:52:24 +0100

    add conditions to update tags only if mismatch

--- main.py
+++ main.py
@@ -9,16 +9,26 @@ for _ ,file in enumerate(files):
     try:
         audiofile = eyed3.load(os.path.join(path, file))
         # Expected file name: Artist - Title.mp3
-        artist = (file.split(".mp3")[0]).split(" - ")[1]
-        title = (file.split(".mp3")[0]).split(" - ")[0]
-        print(artist, "-", title)
-        audiofile.tag.title = artist
-        audiofile.tag.artist = title
-        audiofile.tag.save()
+        title = (file.split(".mp3")[0]).split(" - ")[1]
+        artist = (file.split(".mp3")[0]).split(" - ")[0]
+
+        if audiofile.tag.title != artist or audiofile.tag.artist != title:
+            print(artist, "-", title)
+        
+        if audiofile.tag.title != title:
+            audiofile.tag.title = title
+            audiofile.tag.save()
+        
+        if audiofile.tag.artist != artist:
+            audiofile.tag.artist = artist
+            audiofile.tag.save()
     except:
         errors.append(file)
         continue
 
 if len(errors) > 0:
+    print("------------------")
+    print("Errors:")
+
     for file in files:
         print(file)
